3b134ab44cd8f16cf798c1688be463f662ef388f
heartape <1343348876@qq.com>
2021-11-25 00:53:25 +0800

    日历

--- vue-admin-template/src/layout/components/RightSidebar/index.vue
+++ vue-admin-template/src/layout/components/RightSidebar/index.vue
@@ -1,12 +1,35 @@
 <template>
   <div class="rightSidebar">
-    右边栏
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="250"
+        highlight-current-row="true"
+      />
+    </el-table>
+    <calendar-app />
   </div>
 </template>
 
 <script>
+import CalendarApp from '@/layout/components/RightSidebar/CalendarApp'
 
 export default {
+  components: { CalendarApp },
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-022016-05-022016-05-022016-05-02'
+      }, {
+        date: '2016-05-04'
+      }, {
+        date: '2016-05-01'
+      }, {
+        date: '2016-05-03'
+      }]
+    }
+  }
 }
 </script>
 
