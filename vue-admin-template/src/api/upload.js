eed809a726b0ad658f6e061efa2572d33aedd3b4
heartape <1343348876@qq.com>
2021-12-02 00:49:51 +0800

    前端上传

--- vue-admin-template/src/api/upload.js
+++ vue-admin-template/src/api/upload.js
@@ -14,37 +14,44 @@ export const upload = (item) => {
         // const filename = item.file.uid + item.file.name
         const formData = new FormData()
         const key = ossTokenData.dir + UUID.v4().toString().replace(/-/g, '') + '.png'
+        const keys = ossTokenData.dir
+        const OSSAccessKeyId = ossTokenData.accessid
+        const policy = ossTokenData.policy
+        const signature = ossTokenData.signature
+        const callback = ossTokenData.callback
+        const file = item.file
+        console.log(888888)
+        console.log(key)
+        console.log(keys)
+        console.log(OSSAccessKeyId)
+        console.log(policy)
+        console.log(signature)
+        console.log(callback)
+        console.log(file)
+        console.log(888888)
+        const fileUrl = ossTokenData.host + '/' + key
+        console.log(fileUrl)
         // 注意formData里append添加的键的大小写
-        formData.append('name', item.file.name)
+        // formData.append('name', item.file.name)
         formData.append('key', key) // 存储在oss的文件路径
-        formData.append('OSSAccessKeyId', ossTokenData.accessKeyId) // accessKeyId
+        formData.append('keys', ossTokenData.dir) // 存储在oss的文件路径
+        formData.append('OSSAccessKeyId', ossTokenData.accessid) // accessKeyId
         formData.append('policy', ossTokenData.policy) // policy
         formData.append('signature', ossTokenData.signature) // 签名
         formData.append('callback', ossTokenData.callback) // 签名
         // 如果是base64文件，那么直接把base64字符串转成blob对象进行上传就可以了
         formData.append('file', item.file)
+        formData.append('type', 'image/jpeg')
         formData.append('success_action_status', '200') // 成功后返回的操作码
-        // const formData1 = {
-        //   'name': item.file.name,
-        //   'key': key,
-        //   'OSSAccessKeyId': ossTokenData.accessKeyId,
-        //   'policy': ossTokenData.policy,
-        //   'signature': ossTokenData.signature,
-        //   'callback': ossTokenData.callback,
-        //   'file': item.file,
-        //   'success_action_status': '200'
-        // }
-        const url = ossTokenData.host
-        // const fileUrl = ossTokenData.host + '/' + ossTokenData.dir + filename
+        const url = ossTokenData.host + '/'
+
         console.log(key)
         console.log(url)
         console.log(11111)
         uploadFile(url, formData).then(res => {
-          console.log(22222)
           console.log(res)
           resolve(res)
         }).catch(error => {
-          console.log(333333)
           reject(error)
         })
       }
@@ -52,11 +59,10 @@ export const upload = (item) => {
   })
 }
 // 获取上传通行证
-function getOssToken(params) {
+export function getOssToken() {
   return request({
     url: '/front/upload/picture',
-    method: 'get',
-    params
+    method: 'get'
   })
 }
 
@@ -65,7 +71,7 @@ function uploadFile(url, formData) {
   return request({
     url: url,
     method: 'post',
-    contentType: false, // 必须false才会自动加上正确的Content-Type
+    // contentType: false, // 必须false才会自动加上正确的Content-Type
     formData
   })
 }
--- vue-admin-template/src/components/Tinymce/components/EditorImage.vue
+++ vue-admin-template/src/components/Tinymce/components/EditorImage.vue
@@ -32,6 +32,8 @@
 
 <script>
 import { upload } from '@/api/upload'
+// import axios from 'axios'
+// const UUID = require('uuid')
 
 export default {
   name: 'EditorSlideUpload',
@@ -103,6 +105,46 @@ export default {
       const result = await upload(item)
       console.log(result)
     }
+    // Uploadfile(param) {
+    //   let file = param.file; // 得到文件的内容
+    //   console.log(file);
+    //   //填写获取签名的地址
+    //   const getPolicyApiUrl = 'http://localhost/front/upload/picture'; //获取oss签名的地址
+    //   // 获取oss签名
+    //   axios({
+    //     method: 'get',
+    //     url: getPolicyApiUrl
+    //   }).then((response) => {
+    //     if (response.status === 200) {
+    //       let policyData = response.data.data;
+    //       console.log(policyData);
+    //       /**
+    //        ossUrl 换成自己的Bucket的外网地址，
+    //        例如 https://human-resource-manage.oss-cn-shenzhen.aliyuncs.com
+    //        */
+    //       let ossUrl = 'https://heartape-forum.oss-cn-shanghai.aliyuncs.com';//填写自己OSS服务器的地址
+    //       const uuid = UUID.v4().toString().replace(/-/g, '')
+    //       let accessUrl = policyData.dir + '/' + uuid + file.name;//设置上传的访问路径
+    //       let sendData = new FormData();// 上传文件的data参数
+    //       sendData.append('OSSAccessKeyId', policyData.accessid);
+    //       sendData.append('policy', policyData.policy);
+    //       sendData.append('Signature', policyData.signature);
+    //       sendData.append('keys', policyData.dir);
+    //       sendData.append('key', accessUrl);//上传的文件路径
+    //       sendData.append('success_action_status', 200); // 指定返回的状态码
+    //       sendData.append('type', 'image/jpeg');
+    //       sendData.append('file', file);
+    //       console.log(sendData);
+    //       axios.post(
+    //         ossUrl,
+    //         sendData
+    //       ).then((res) => {
+    //         this.pictureUrl = ossUrl + '/' + accessUrl;//获得到的url需要将其存数据库中
+    //         console.log('上传到阿里云的图片地址：' + ossUrl + '/' + accessUrl)
+    //       })
+    //     }
+    //   })
+    // }
   }
 }
 </script>
