e83fe363432ac1dc77f6cc57f312d953c0d6dfe7
heartape <1343348876@qq.com>
2021-12-15 18:11:48 +0800

    开发

--- heartape-forum-vue/src/views/article/publish.vue
+++ heartape-forum-vue/src/views/article/publish.vue
@@ -14,7 +14,30 @@
       >
         <template slot="prepend">标题</template>
       </el-input>
-      <h3>标签</h3>
+      <div class="sort-container">
+        <span class="sort-title">标签</span>
+        <el-tag
+          v-for="item in sortChoose"
+          :key="item.sortId"
+          closable
+          @close="handleClose(item)"
+        >{{ item.name }}</el-tag>
+        <div v-if="sortChoose.length < maxSortNumber" class="sort-add-container">
+          <el-autocomplete
+            v-if="inputVisible"
+            ref="saveTagInput"
+            v-model="sortInput.value"
+            class="input-new-sort"
+            size="small"
+            :fetch-suggestions="querySearch"
+            @select="handleSelect"
+            @keyup.enter.native="handleInputConfirm"
+          />
+          <el-button v-if="inputVisible" class="button-add-sort" size="small" @click="handleInputConfirm">添加</el-button>
+          <el-button v-if="inputVisible" class="button-leave-sort" size="small" @click="leaveInputConfirm">取消</el-button>
+          <el-button v-else class="button-new-sort" size="small" @click="showInput">+ 标签</el-button>
+        </div>
+      </div>
       <tinymce v-model="article.content" :width="1000" @imagesUpload="imagesUpload(arguments)" />
     </div>
   </div>
@@ -37,10 +60,59 @@ export default {
         content: `<h1 style="text-align: center;">Welcome to the TinyMCE demo!</h1>`
       },
       // todo:标签选择
-      sort: []
+      inputVisible: false,
+      sortInput: {},
+      sort: [],
+      sortChoose: [
+        { sortId: 1, name: '计算机' }
+      ],
+      maxSortNumber: 5
     }
   },
+  created() {
+    this.sort = [
+      { sortId: 1, value: '计算机' },
+      { sortId: 2, value: '数学' },
+      { sortId: 3, value: '云计算' },
+      { sortId: 4, value: '微服务' }
+    ]
+  },
   methods: {
+    handleClose(sort) {
+      this.sortChoose.splice(this.sortChoose.indexOf(sort), 1)
+    },
+    showInput() {
+      this.inputVisible = true
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus()
+      })
+    },
+    // sort模糊搜索
+    querySearch(queryString, cb) {
+      const sort = this.sort
+      const result = queryString ? sort.filter(item => item.value.match(queryString)) : sort
+      // const result = queryString ? sort.filter(item => item.value.match('/^.*' + queryString + '.*$/')) : sort
+      cb(result)
+    },
+    // 选择
+    handleSelect(item) {
+      this.sortInput = item
+    },
+    // 执行添加
+    handleInputConfirm() {
+      const sortId = this.sortInput.sortId
+      if (sortId) {
+        // 存入sort
+        this.sortInput.name = this.sortInput.value
+        this.sortChoose.push(this.sortInput)
+      }
+      this.inputVisible = false
+      this.sortInput = {}
+    },
+    leaveInputConfirm() {
+      this.inputVisible = false
+      this.sortInput = {}
+    },
     // 整合oss上传
     imagesUpload: function(params) {
       const blobInfo = params[0]
@@ -99,7 +171,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .article-publish-container {
   width: 1000px;
   margin: 20px auto;
@@ -110,6 +182,42 @@ export default {
   font-size: 25px;
 }
 .el-input {
-  margin-bottom: 40px;
+  margin-bottom: 10px;
 }
+
+.sort-container {
+  margin-bottom: 10px;
+  background-color: #ffffff;
+  .sort-title {
+    padding: 8px 20px;
+    background-color: #F5F7FA;
+    color: #909399;
+    font-size: 14px;
+    margin-right: 10px;
+    border: #DCDFE6 solid 1px;
+  }
+  .el-tag {
+    margin-right: 10px;
+  }
+  .sort-add-container {
+    display: inline-block;
+    .button-new-sort {
+      margin-left: 10px;
+      height: 32px;
+      width: 74px;
+      line-height: 30px;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
+    .input-new-sort {
+      width: 90px;
+      margin-left: 10px;
+      vertical-align: bottom;
+    }
+    .el-button {
+      margin: 0;
+    }
+  }
+}
+
 </style>
